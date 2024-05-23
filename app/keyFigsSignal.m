34976eb430a9aed42424da3bee4c82fa18f005e4
ronjala96 <131967906+ronjala96@users.noreply.github.com>
2024-05-23 18:06:01 +0200

    Add files via upload

--- app/keyFigsSignal.m
+++ app/keyFigsSignal.m
@@ -1,30 +1,49 @@
-function [numOfComps_sig,meanPComp_sig,varPComp_sig] = keyFigsSignal(signal, windowSize, stepSize, samp_freq, thresh)
+function [numOfComps_sig,meanPComp_sig,varPComp_sig,stftPComp_sig,freqs] = keyFigsSignal(signal, windowSize, stepSize, sampFreq, thresh)
     [m,n] = size(signal);
     end_idx = int32(floor((m - windowSize) / stepSize)) + 1;
     allMeanPComps = cell(1,end_idx);
     allVarPComps = cell(1,end_idx);
+    allStftPComp = cell(1,end_idx);
     f = waitbar(0,'1','Name','Analysing Signal');
     for k=1:end_idx
         waitbar(double(k)/double(end_idx),f,'Processing Analysis...');
         start_win = 1+int32((k-1) * stepSize);
         end_win = int32(start_win + windowSize)-1;
         win = signal(start_win:end_win,:);
-        comps = SSD(win, samp_freq, thresh);
+        comps = SSD(win, sampFreq, thresh);
         comps = comps.';
-        [numOfComps,meanPComp,varPComp] = keyFigsWindow(comps);
+        [numOfComps,meanPComp,varPComp,stftPComp,freqs] = keyFigsWindow(comps,sampFreq, windowSize);
         numOfComps_sig(k) = numOfComps;
         allMeanPComps{k} = meanPComp;
         allVarPComps{k} = varPComp;
+        allStftPComp{k} = stftPComp;
     end
     maxNumComps = max(numOfComps_sig);
     
     meanPComp_sig = NaN(end_idx,maxNumComps);
     varPComp_sig = NaN(end_idx,maxNumComps);
-
+    stftPComp_sig = cell(maxNumComps,1);
+    for n=1:maxNumComps
+        stftPComp_sig{n} = [];
+    end
     for i=1:end_idx
         waitbar(double(i)/double(end_idx),f,'Cleaning Results...')
         meanPComp_sig(i,1:length(allMeanPComps{i})) = allMeanPComps{i};
         varPComp_sig(i,1:length(allVarPComps{i})) = allVarPComps{i};
+        % get frequencies of window per component
+        stftComps = allStftPComp{i};
+
+        % add the components frequencies to frequency matrix
+        for n=1:maxNumComps
+            if n <= size(stftComps,2)
+                % if component with index n exists, add frequencys to the
+                % matrix
+                stftComp_temp = stftComps(:,n);
+                stftPComp_sig{n} = [stftPComp_sig{n} stftComp_temp];
+            else
+                stftPComp_sig{n} = [stftPComp_sig{n} zeros(size(stftComps,1),1)];
+            end
+        end
     end
 
     close(f)
--- app/keyFigsWindow.m
+++ app/keyFigsWindow.m
@@ -1,4 +1,4 @@
-function [numOfComps,meanPComp,varPComp] = keyFigsWindow(components)
+function [numOfComps,meanPComp,varPComp,stftPComp,freqs] = keyFigsWindow(components,samp_freq,windowSize)
 % Function to calculate key figures for the components that are the results
 % of the SSD function. The results are ordered by decreasng frequency, that
 % means the first mean corresponds to the result with the highest
@@ -9,8 +9,16 @@ function [numOfComps,meanPComp,varPComp] = keyFigsWindow(components)
 
     numOfComps = size(components,2);
     for i=1:numOfComps
-        comp = components(i,:);
+        if i==7
+            a=0;
+        end
+        comp = components(:,i);
         meanPComp(i) = mean(comp);
         varPComp(i) = var(comp);
+        fs = double(samp_freq);
+        w = windowSize;
+        no = w / 2; % Overlap
+        [s,freqs,t] = spectrogram(comp, w, no, [], samp_freq);
+        stftPComp(:,i) = s;
     end
 end
