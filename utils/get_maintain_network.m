f9043731e0667d3907817e4aafd941291c48a075
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2024-06-13 21:09:50 +0200

    Update get_maintain_network.m

--- utils/get_maintain_network.m
+++ utils/get_maintain_network.m
@@ -6,8 +6,5 @@ function model = get_maintain_network(number_of_classes)
         insert(py.sys.path, int32(0), path_to_network);
     end
 
-    torch = py.importlib.import_module('torch');
-    torch.backends.mkl.enabled = true;
-
     model = py.MaintainNet.MaintainNet("MrMaintenance", int32(number_of_classes));
 end
