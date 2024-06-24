45193398938749b2c15f3e8d6c959948f7b91594
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2024-06-24 10:26:47 +0200

    Refactor data generator

--- data_generation/DataGenerator.m
+++ data_generation/DataGenerator.m
@@ -77,25 +77,7 @@ classdef DataGenerator
             if fault_flag || rand() <= obj.fault_probability
                 [faults, specific_fault_type] = obj.generate_specific_faults(time);
                 faulty_signal = faulty_signal + faults;
-                fault_type = obj.update_fault_type(fault_type, specific_fault_type);
-            end
-        end
-
-        function fault_type = update_fault_type(obj, previous_fault_type, new_fault_type)
-            if previous_fault_type == FaultTypes.HEALTHY
-                fault_type = new_fault_type;
-            elseif previous_fault_type == FaultTypes.IMPULSE
-                if new_fault_type == FaultTypes.ADDITIONAL_COMPONENT
-                    fault_type = FaultTypes.IMPULSE_AND_ADDITIONAL_COMPONENT;
-                elseif new_fault_type == FaultTypes.FREQUENCY_MODULATION
-                    fault_type = FaultTypes.IMPULSE_AND_FREQUENCY_MODULATION;
-                elseif new_fault_type == FaultTypes.AMPLITUDE_MODULATION
-                    fault_type = FaultTypes.IMPULSE_AND_AMPLITUDE_MODULATION;
-                elseif new_fault_type == FaultTypes.FREQUENCY_AND_AMPLITUDE_MODULATION
-                    fault_type = FaultTypes.IMPULSE_AND_FREQUENCY_AND_AMPLITUDE_MODULATION;
-                end
-            else
-                error("Faut type %s not suported", previous_fault_type)
+                fault_type = update_fault_type(fault_type, specific_fault_type);
             end
         end
     end
---
+++ data_generation/utils/update_fault_type.m
@@ -0,0 +1,17 @@
+function fault_type = update_fault_type(previous_fault_type, new_fault_type)
+    if previous_fault_type == FaultTypes.HEALTHY
+        fault_type = new_fault_type;
+    elseif previous_fault_type == FaultTypes.IMPULSE
+        if new_fault_type == FaultTypes.ADDITIONAL_COMPONENT
+            fault_type = FaultTypes.IMPULSE_AND_ADDITIONAL_COMPONENT;
+        elseif new_fault_type == FaultTypes.FREQUENCY_MODULATION
+            fault_type = FaultTypes.IMPULSE_AND_FREQUENCY_MODULATION;
+        elseif new_fault_type == FaultTypes.AMPLITUDE_MODULATION
+            fault_type = FaultTypes.IMPULSE_AND_AMPLITUDE_MODULATION;
+        elseif new_fault_type == FaultTypes.FREQUENCY_AND_AMPLITUDE_MODULATION
+            fault_type = FaultTypes.IMPULSE_AND_FREQUENCY_AND_AMPLITUDE_MODULATION;
+        end
+    else
+        error("Faut type %s not suported", previous_fault_type)
+    end
+end
