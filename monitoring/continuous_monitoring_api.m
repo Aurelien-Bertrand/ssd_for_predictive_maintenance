42fe5a57199df22faa86f594056d930596d69b2e
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2024-06-24 10:29:27 +0200

    Update continuous_monitoring_api.m

--- monitoring/continuous_monitoring_api.m
+++ monitoring/continuous_monitoring_api.m
@@ -1,6 +1,3 @@
-% TODO: could use a 0.5Hz sampling interval and a 10-minute averaging interval
-% TODO: somehow relate to SCADA - so 10 minutes averages?
-
 addpath ./utils/
 addpath ./data_generation/
 
@@ -10,14 +7,13 @@ WINDOW_SIZE = 1000; % Size of the window
 SAMPLING_FREQUENCY = 10; % Amount of samples per second
 SIGNAL_TO_NOISE_RATIO = 0;
 
-number_of_hours = 10;
+number_of_hours = 0.2; % TODO: this is a parameter that the user should give (could be in minutes)
 total_number_of_seconds = number_of_hours * 60 * 60;
 
 total_samples = total_number_of_seconds * SAMPLING_FREQUENCY;
 time = linspace(0, total_number_of_seconds, total_samples);
 
 generator = RealisticGenerator(SAMPLING_FREQUENCY, SIGNAL_TO_NOISE_RATIO);
-% model = get_maintain_network(2);
 
 for i = 1:WINDOW_SIZE:length(time)-WINDOW_SIZE+1
     window_time = time(i:i+WINDOW_SIZE-1);
@@ -27,15 +23,9 @@ for i = 1:WINDOW_SIZE:length(time)-WINDOW_SIZE+1
     
     data = generator.generate_dataset(1, window_time);
 
-    % Convert the array to a JSON string
     jsonData = jsonencode(struct('array', data.signals));
-
-    % Set the options for the webwrite function
     options = weboptions('MediaType', 'application/json');
-
-    % Send the JSON data to the Flask server
     response = webwrite(url, jsonData, options);
-
 
     fault = string(response.result);
 
