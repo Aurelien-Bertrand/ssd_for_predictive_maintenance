b66145ad204bc1ec0b5a1fe11ba868c131134f97
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2024-06-24 10:27:00 +0200

    Update _map_class_index_to_name.py

--- faults_classification/_map_class_index_to_name.py
+++ faults_classification/_map_class_index_to_name.py
@@ -1,7 +1,19 @@
 _CLASS_INDEX_TO_NAME = {
+    # Binary
     0: "healthy",
     1: "faulty",
-} # TODO: edit this for multi-class later
+    
+    # Multi-class
+    2: "impulse",
+    3: "additional component",
+    4: "frequency modulation",
+    5: "amplitude modulation",
+    6: "frequency and amplitude modulation",
+    7: "impulse and additional component",
+    8: "impulse and frequency modulation",
+    9: "impulse and amplitude modulation",
+    10: "impulse, frequency and amplitude modulation"
+}
 
 def map_class_index_to_name(class_index: int) -> str:
     assert class_index in _CLASS_INDEX_TO_NAME.keys(), f"Class {class_index} is not known."
