e1c2c2a03ebbc900d4f580fb988c1a619f8fcb50
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2024-06-26 13:38:28 +0200

    Update get_model.py

--- faults_classification/get_model.py
+++ faults_classification/get_model.py
@@ -3,8 +3,17 @@ from faults_classification.MaintainNet import MaintainNet
 
 CACHE_PATH = "_cache/models/"
 
-def get_model(model_name="simplemodel.pth"):
-    model = MaintainNet('MrMaintenance', 4)
+SIMPLE_MODEL_NAME = "simplemodel.pth"
+SIMPLE_MODEL_NUM_CLASSES = 4
+
+REALISTIC_MODEL_NAME = "realisticmodel.pth"
+REALISTIC_MODEL_NUM_CLASSES = 8
+
+def get_model(use_realistic_data: bool = True):
+    num_classes = REALISTIC_MODEL_NUM_CLASSES if use_realistic_data else SIMPLE_MODEL_NUM_CLASSES
+    model_name = REALISTIC_MODEL_NAME if use_realistic_data else SIMPLE_MODEL_NAME
+        
+    model = MaintainNet(name="MrMaintenance", num_classes=num_classes)
     model.load_state_dict(torch.load(CACHE_PATH + model_name))
     model.eval()
     
