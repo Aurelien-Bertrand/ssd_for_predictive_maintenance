9edd12b65965badebda67ec379e7043c031396cc
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2024-06-26 14:15:59 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
+import argparse
 from flask import Flask, request, jsonify
 from faults_classification.MaintainNet import MaintainNet
 import math
 from faults_classification.get_model import get_model
 
+parser = argparse.ArgumentParser(description="Run the Flask server with boolean flags.")
+parser.add_argument("use_realistic_data", type=lambda x: (str(x).lower() == "true"))
+args = parser.parse_args()
+
 app = Flask(__name__)
 
-model = get_model()
+model = get_model(args.use_realistic_data)
 
 @app.route("/predict", methods=["POST"])
 def predict():
